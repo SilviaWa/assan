c1741dd072f8e95c78c0a97f3901d07de5522330
bovetliu <bowei_liu@ymail.com>
2015-10-03 09:46:40 -0500

    save

---
+++ assan-v1.7.5/scss/copystyle.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Program:
+#    This program shows "Hello World" in your screen.
+#History
+
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+cp style.css -t ../css -f
+cp style.css.map -t ../css -f
+
+exit 0
